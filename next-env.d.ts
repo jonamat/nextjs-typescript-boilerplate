572736e1cb342311a93e3a9a434a2deb8357de5a
Jonathan Mataloni <jo.mataloni@gmail.com>
2021-06-15 15:46:25 +0200

    Prepare dev env

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      SECRET: string;
+    }
+  }
+}
